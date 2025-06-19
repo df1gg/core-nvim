b84cb9ea6fdfe99325186dc174e5736d8e7a1385
df1gg <samsung26032002@gmail.com>
2025-06-19 20:13:03 +0300

    basic settings nvim

---
+++ lua/config/options.lua
@@ -0,0 +1,33 @@
+-- lua/config/options.lua
+
+
+local opt = vim.opt
+
+-- Main
+opt.number = true
+opt.relativenumber = true
+opt.mouse = "a"
+opt.clipboard = "unnamedplus"
+opt.swapfile = false
+opt.backup = false
+opt.undofile = true
+opt.termguicolors = true
+opt.cursorline = true
+
+-- Tabs
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
+
+-- Search
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = true
+opt.incsearch = true
+
+-- Interface
+opt.scrolloff = 8
+opt.signcolumn = "yes"
+opt.updatetime = 250
+opt.timeoutlen = 300
