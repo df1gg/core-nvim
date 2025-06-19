b431f59fe7ebbdfbcc775d71ea6874b95e0200a5
df1gg <samsung26032002@gmail.com>
2025-06-19 23:00:35 +0300

    add main menu

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -250,4 +250,33 @@ require("lazy").setup({
             })
         end,
     },
+    {
+        "goolord/alpha-nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            local alpha = require("alpha")
+            local dashboard = require("alpha.themes.dashboard")
+
+            dashboard.section.header.val = {
+                "███╗   ██╗██╗   ██╗██╗███╗   ███╗",
+                "████╗  ██║██║   ██║██║████╗ ████║",
+                "██╔██╗ ██║██║   ██║██║██╔████╔██║",
+                "██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║",
+                "██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║",
+                "╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝",
+            }
+
+            dashboard.section.buttons.val = {
+                dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+                dashboard.button("f", "󰱼  Search files", ":Telescope find_files<CR>"),
+                dashboard.button("r", "  Last files", ":Telescope oldfiles<CR>"),
+                dashboard.button("q", "  Exit", ":qa<CR>"),
+            }
+
+            dashboard.section.footer.val = "neovim ✨ minimal but powerful"
+            dashboard.opts.opts.noautocmd = true
+
+            alpha.setup(dashboard.opts)
+        end,
+    },
 })
