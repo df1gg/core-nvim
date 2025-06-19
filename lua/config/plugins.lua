c0ffe399e479bc90c3cd95bad515936e6ff87360
df1gg <samsung26032002@gmail.com>
2025-06-19 21:53:58 +0300

    add Telescope

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -120,5 +120,26 @@ require("lazy").setup({
             })
         end
     },
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        cmd = "Telescope",
+        keys = {
+            { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+            { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+            { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find Buffers" },
+            { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
+        },
+        config = function()
+            require("telescope").setup({
+                defaults = {
+                    layout_strategy = "horizontal",
+                    layout_config = { width = 0.9 },
+                    sorting_strategy = "ascending",
+                    prompt_prefix = "🔍 ",
+                },
+            })
+        end,
+    },
 })
 
